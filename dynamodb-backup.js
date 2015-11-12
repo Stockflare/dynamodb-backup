f452fc5216f10ea3a864bb29a95a5b3ca46aa27e
Mark Stratmann <mark@stratmann.me.uk>
2015-11-12 21:26:00 +0000

    Better handling of errors

--- dynamodb-backup.js
+++ dynamodb-backup.js
@@ -111,12 +111,15 @@ dynamodb.scan(params, onScan);
 function onScan(err, data) {
     if (err) {
         console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+        logger.log("Unable to scan the table. Error JSON: " + JSON.stringify(err, null, 2));
         // ON provisioned throughput error then just pause and try again
         if (err.code === 'ProvisionedThroughputExceededException' || err.code === 'ThrottlingException') {
           Fiber(function() {
             sleep(10000);
           }).run();
           dynamodb.scan(params, onScan);
+        } else {
+          exit(1);
         }
 
     } else {
