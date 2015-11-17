28c7f1956b67e00cdfecd02dea55afe2800a94f1
Mark Stratmann <mark@stratmann.me.uk>
2015-11-17 20:32:29 +0000

    Changes to enable ECS Task launching

--- dynamodb-backup.js
+++ dynamodb-backup.js
@@ -7,6 +7,7 @@ var region_env = process.env.AWS_REGION;
 var bucket_env  = process.env.BACKUP_FILE_BUCKET;
 var log_group_env = process.env.CLOUDWATCH_LOG_GROUP;
 var log_stream_env = process.env.CLOUDWATCH_LOG_STREAM;
+var table_name_env = process.env.TABLE_NAME;
 var AWS = require('aws-sdk');
 var _ = require('underscore');
 var fs = require('fs');
@@ -25,11 +26,11 @@ var lawgs = require('./aws_logger');
 
 program
   .version('0.0.1')
-  .option('-t, --table [string]', 'DynamoDB Table to backup')
+  .option('-t, --table [string]', 'DynamoDB Table to backup, defaults to ${TABLE_NAME}', table_name_env)
   .option('-b, --bucket [string]', 'Bucket to upload backup file to, defaults to  ${BACKUP_FILE_BUCKET}', bucket_env)
   .option('-l, --log-group [string]', 'CloudWatch Log Group to receive log entries, defaults to ${CLOUDWATCH_LOG_GROUP}', log_group_env)
   .option('-s, --log-stream [string]', 'CloudWatch Log Stream to receive log entries, defaults to ${CLOUDWATCH_LOG_STREAM}', log_stream_env)
-  .option('-e, --encode', 'Encode the row data in Base64')
+  .option('-e, --encode', 'Encode the row data in Base64, defaults to true', true)
   .option('-r, --region', 'Region for AWS API calls, defaults to ${AWS_REGION}', region_env)
   .parse(process.argv);
 
